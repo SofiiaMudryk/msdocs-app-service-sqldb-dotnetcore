bb51e92b456cd4cb5af8a6a16680d1489e9cef63
Sofiia Mudryk <168586099+SofiiaMudryk@users.noreply.github.com>
2026-05-28 04:45:14 +0300

    Configure MySQL and Redis

--- Program.cs
+++ Program.cs
@@ -1,25 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using DotNetCoreSqlDb.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add database context and cache
-if(builder.Environment.IsDevelopment())
+if (builder.Environment.IsDevelopment())
 {
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
+        options.UseMySql(
+            builder.Configuration.GetConnectionString("MyDbConnection"),
+            ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("MyDbConnection"))
+        ));
+
     builder.Services.AddDistributedMemoryCache();
 }
 else
 {
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
+        options.UseMySql(
+            builder.Configuration["AZURE_MYSQL_CONNECTIONSTRING"],
+            ServerVersion.AutoDetect(builder.Configuration["AZURE_MYSQL_CONNECTIONSTRING"])
+        ));
+
     builder.Services.AddStackExchangeRedisCache(options =>
     {
-    options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
-    options.InstanceName = "SampleInstance";
+        options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
+        options.InstanceName = "SampleInstance";
     });
 }
-
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -33,7 +41,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
